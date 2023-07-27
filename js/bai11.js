5774d680b362da6c062ea131cda14ddc7d9ff40b
Sprite-Nguyen241 <140497062+Sprite-Nguyen241@users.noreply.github.com>
2023-07-27 15:55:40 +0700

    Add files via upload

---
+++ js/bai11.js
@@ -0,0 +1,10 @@
+// JavaScript Document
+var typed = new Typed('.element', {
+strings: ["Web Designer.", "Code Developer.","Apps Designer.","World Maker"],
+typeSpeed:30,
+backSpeed:30,
+loop:true,
+});
+$('.counter').countUp();
+
+new WOW().init();
